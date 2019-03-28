8f3483304f7cae6f98a6ab0b3eddd1869949ad77
sqh <812411866@qq.com>
2019-03-28 23:58:35 +0800

    add data_structures dir

---
+++ src/data_structures/TreeaNode.java
@@ -0,0 +1,15 @@
+package data_structures;
+
+import javax.swing.tree.TreeNode;
+
+public class TreeaNode {
+	Object element;
+	TreeNode left;
+	TreeNode right;
+}
+
+class BinaryNode {
+	Object element;
+	BinaryNode left;
+	BinaryNode right;
+}
