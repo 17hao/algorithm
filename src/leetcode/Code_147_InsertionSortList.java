1d109bd09e2530462819475641aea25a4b83b8e1
17hao <812411866@qq.com>
2019-04-07 20:49:23 +0800

    finish No.147

--- src/leetcode/Code_147_InsertionSortList.java
+++ src/leetcode/Code_147_InsertionSortList.java
@@ -4,43 +4,48 @@ package leetcode;
  * 对链表进行插入排序
  *
  * @author sqh
- * @since 2018/12/18
  * @finish 2019/4/7
+ * @since 2018/12/18
  */
 public class Code_147_InsertionSortList {
     static ListNode insertLinkedList(ListNode head) {
-        if (head == null && head.next == null) {
+        if (head == null || head.next == null) {
             return head;
         }
         ListNode dummyHead = new ListNode(Integer.MIN_VALUE); //链表的哨兵节点
         dummyHead.next = head;
-        ListNode cur = head; //负责查找需要插入的节点
+        ListNode cur = head; //负责找到需要插入的节点
         while (cur.next != null) {
             if (cur.val < cur.next.val) {
-                cur = cur.next;
+                cur = cur.next;  //cur.next就是需要插入的元素
+            } else {
+                ListNode tmp = cur.next;
+                ListNode d = dummyHead;
+                cur.next = cur.next.next;
+                while (d.next.val < tmp.val) {
+                    d = d.next;
+                }
+                tmp.next = d.next;
+                d.next = tmp;
             }
-            ListNode tmp = cur.next, q = dummyHead;
-            cur.next = cur.next.next;
-            while (q.next.val < tmp.val) {
-                q = q.next;
-            }
-            tmp.next = q.next;
-            q.next = tmp;
         }
         return dummyHead.next;
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(-1);
-        ListNode one = new ListNode(5);
-        ListNode two = new ListNode(3);
-        ListNode three = new ListNode(4);
-        ListNode four = new ListNode(0);
-        head.next = one;
-        one.next = two;
-        two.next = three;
-        three.next = four;
-        insertLinkedList(head);
-        System.out.println(head.val + " -> " + head.next.val + " -> " + head.next.next.val + " -> " + head.next.next.next.val + " -> " + head.next.next.next.next.val);
+        int[] arr = {3, 2, 4};
+        //int[] arr = {4, 3, 2, 5, 6, 7, 1, 0};
+        //int[] arr = {-1, 5, 3, 4, 0};
+        ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
+        ListNode cur = dummyHead;
+        for (int value : arr) {
+            cur.next = new ListNode(value);
+            cur = cur.next;
+        }
+        ListNode realHead = insertLinkedList(dummyHead.next);
+        while (realHead != null) {
+            System.out.print(realHead.val + "->");
+            realHead = realHead.next;
+        }
     }
 }
