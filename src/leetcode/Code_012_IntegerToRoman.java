7f206e7c76dee4b04bd0dc17e22ce31018499b1a
17hao <812411866@qq.com>
2019-04-11 20:45:42 +0800

    leetcode No.12

--- src/leetcode/Code_012_IntegerToRoman.java
+++ src/leetcode/Code_012_IntegerToRoman.java
@@ -9,17 +9,25 @@ import java.util.Scanner;
  * @date 2018/12/19
  */
 public class Code_012_IntegerToRoman {
-	private static Scanner scanner;
+    private static Scanner scanner;
 
-	public static String intToRoman(int num) {
-		String res = new String();
-		return res;
-	}
+    public static String intToRoman(int num) {
+        String res = new String();
+        int[] values = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] reps = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        for (int i = 12; i >= 0; i--) {
+            while (num >= values[i]) {
+                num -= values[i];
+                res += reps[i];
+            }
+        }
+        return res;
+    }
 
-	public static void main(String[] args) {
-		scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		String res = intToRoman(num);
-		System.out.print("结果是:" + res);
-	}
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String res = intToRoman(num);
+        System.out.print("结果是:" + res);
+    }
 }
