4c05ad1e06f4f26178430a20c4c8f0da74a2b4e3
17hao <812411866@qq.com>
2019-04-13 10:02:28 +0800

    leetcode No.13

--- src/leetcode/Code_013_RomanToInteger.java
+++ src/leetcode/Code_013_RomanToInteger.java
@@ -7,24 +7,28 @@ import java.util.HashMap;
  * @date 2019-04-12 19:44
  */
 public class Code_013_RomanToInteger {
-    static int romanToInt(String s) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        hm.put("I", 1);
-        hm.put("V", 5);
-        hm.put("X", 10);
-        hm.put("L", 50);
-        hm.put("C", 100);
-        hm.put("D", 500);
-        hm.put("M", 1000);
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if ()
+    private static int romanToInt(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        hm.put('I', 1);
+        hm.put('V', 5);
+        hm.put('X', 10);
+        hm.put('L', 50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
+        int res = hm.get(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (hm.get(s.charAt(i)) > hm.get(s.charAt(i - 1))) {
+                res += (hm.get(s.charAt(i)) - 2 * hm.get(s.charAt(i - 1)));
+            } else {
+                res += hm.get(s.charAt(i));
+            }
         }
         return res;
     }
 
     public static void main(String[] args) {
-        String str = "M";
+        String str = "MCMXCIV";
         int res = romanToInt(str);
         System.out.println(res);
     }
