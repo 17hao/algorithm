42c72f57e5f2b5d96319dfb3bfb92f164073b1af
17hao <812411866@qq.com>
2019-04-20 17:37:44 +0800

    LeetCode No.53 && No.121

---
+++ src/leetcode/Code_053_MaximumSubarray.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+/**
+ * @author 17hao
+ * @date 2019-04-20 15:04
+ */
+public class Code_053_MaximumSubarray {
+    /**
+     * 问题的关键在于维护2个值：
+     * ①当前位置之前的序列的最大连续子序列最大值
+     * ②'当前位置值'和'当前位置之前序列的最大连续子序列之和'的较大值
+     */
+    private static int maxSubarray(int[] nums) {
+        int maxSum, maxNow;
+        maxSum = maxNow = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (maxNow < 0)
+                maxNow = nums[i];
+            else
+                maxNow += nums[i];
+            maxSum = maxSum > maxNow ? maxSum : maxNow;
+        }
+        return maxSum;
+    }
+
+    private static int maxSubarrayV2(int[] nums) {
+        int maxSum, maxNow;
+        maxSum = maxNow = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxNow = Math.max(nums[i], maxNow + nums[i]);
+            maxSum = Math.max(maxNow, maxSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int res = maxSubarrayV2(nums);
+        System.out.println(res);
+    }
+}
---
+++ src/leetcode/Code_121_BestTimeBuyStock.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * @author 17hao
+ * @date 2019-04-20 17:12
+ */
+public class Code_121_BestTimeBuyStock {
+    /* max(前i-1天最大收益，第i天价格 - 前i-1天最低价) */
+    private static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = prices.length > 0 ? prices[0] : 0;
+        for (int i = 1; i < prices.length; i++) {
+            minPrice = Math.min(prices[i], minPrice);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {};
+        //int[]prices = {7, 1, 5, 3, 6, 4};
+        int res = maxProfit(prices);
+        System.out.println(res);
+    }
+}
