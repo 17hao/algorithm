38d6320dfc18349e9ff21b63cbbc17fd282a9e31
ShiQihao <ShiQihao>
2019-04-21 10:16:39 +0800

    LeetCode No.203

--- src/tool/LinkedListTool.java
+++ src/tool/LinkedListTool.java
@@ -2,18 +2,28 @@ package tool;
 
 /**
  * @author 17hao
- * @since 2019/4/7
+ * @since 2019-04-07
  */
 public class LinkedListTool {
-    public static ListNode generateList(int[] arr) {
+    /**
+     * generate a linked list by given array
+     * @param array
+     * @return the head of linked list
+     */
+    public static ListNode generateList(int[] array) {
         ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
         ListNode cur = dummyHead;
-        for (int value : arr) {
+        for (int value : array) {
             cur.next = new ListNode(value);
             cur = cur.next;
         }
         return dummyHead.next;
     }
+
+    /**
+     * print linked list
+     * @param head
+     */
     public static void printList(ListNode head) {
         while (head != null) {
             System.out.print(head.val + "->");
