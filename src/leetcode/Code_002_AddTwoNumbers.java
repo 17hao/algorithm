f875d6569677c629b7dc4b5328b73fe2ea0bfc02
17hao <812411866@qq.com>
2019-04-29 15:54:41 +0800

    LeetCode No.2

--- src/leetcode/Code_002_AddTwoNumbers.java
+++ src/leetcode/Code_002_AddTwoNumbers.java
@@ -3,62 +3,41 @@ package leetcode;
 import tool.LinkedListTool;
 import tool.ListNode;
 
-import java.util.LinkedList;
-
 /**
  * @author 17hao
  * @date 2019-04-12 16:50
+ * finished at 2019-4-29 15:10
  */
 public class Code_002_AddTwoNumbers {
-    //todo big integer unsolved
+    //todo practice again!
     private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // int num1 = listToNum(l1);
-        // int num2 = listToNum(l2);
-        // int res = num1 + num2;
-        // // if (res > Integer.MAX_VALUE){
-        // //
-        // // }
-        // ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
-        // ListNode cur = dummyHead;
-        // if (res == 0) {
-        //     dummyHead.next = new ListNode(0);
-        // } else {
-        //     while (res > 0) {
-        //         cur.next = new ListNode(res % 10);
-        //         cur = cur.next;
-        //         res /= 10;
-        //     }
-        // }
-        // return dummyHead.next;
-        ListNode dummyNode = new ListNode(Integer.MIN_VALUE);
-        ListNode cur = dummyNode;
-        while (l1 != null || l2 != null) {
-            int tmp = l1.val + l2.val;
-            cur.next = new ListNode(tmp >= 10 ? tmp - 10 : tmp);
+        ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
+        ListNode cur = dummyHead;
+        boolean f = false;
+        while (l1 != null && l2 != null) {
+            int temp = !f ? l1.val + l2.val : l1.val + l2.val + 1;
+            f = temp >= 10;
+            cur.next = new ListNode(temp % 10);
             cur = cur.next;
             l1 = l1.next;
             l2 = l2.next;
         }
-        return dummyNode.next;
-    }
-
-    private static int listToNum(ListNode l1) {
-        LinkedList<Integer> list1 = new LinkedList<>();
-        while (l1 != null) {
-            list1.add(l1.val);
-            l1 = l1.next;
+        ListNode continuous = l1 == null ? l2 : l1; //选出需要继续遍历的链表
+        while (continuous != null) {
+            int temp = f ? 1 + continuous.val : continuous.val;
+            f = temp >= 10;
+            temp = temp >= 10 ? temp % 10 : temp;
+            cur.next = new ListNode(temp);
+            cur = cur.next;
+            continuous = continuous.next;
         }
-        int num = 0;
-        for (int i = list1.size() - 1; i >= 0; i--) {
-            num += list1.get(i) * Math.pow(10, i);
-        }
-        return num;
+        if (f) cur.next = new ListNode(1);
+        return dummyHead.next;
     }
 
     public static void main(String[] args) {
-        int[] arr1 = {9};
-        int[] arr2 = {1};
-        //int[] arr2 = {1, 9, 9, 9, 9, 9, 9, 9, 9};
+        int[] arr1 = {3};
+        int[] arr2 = {9, 2};
         ListNode l1 = LinkedListTool.generateList(arr1);
         ListNode l2 = LinkedListTool.generateList(arr2);
         ListNode res = addTwoNumbers(l1, l2);
