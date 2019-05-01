473ef0ad0e65cb9ed7170f59123d832249787fb5
17hao <812411866@qq.com>
2019-05-01 16:14:21 +0800

    LeetCode No.237 && No.292

---
+++ src/leetcode/Code_237_DeleteNode.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import tool.LinkedListTool;
+import tool.ListNode;
+
+/**
+ * @author 17hao
+ * @date 2019-05-01 15:58
+ */
+public class Code_237_DeleteNode {
+    private static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        ListNode head = LinkedListTool.generateList(arr);
+        deleteNode(head.next);
+        LinkedListTool.printList(head);
+    }
+}
