4f40eb0852facd7be535d40a1012358b5d51cc7a
17hao <sqh1107@gmail.com>
2019-12-17 19:57:39 +0800

    1st: 122 && 123

---
+++ src/leetcode/first/Code_122_BestTimeBuyStock2.java
@@ -0,0 +1,41 @@
+package leetcode.first;
+
+/**
+ * 股票买卖2, 可以无限次交易,求最大利润.
+ * 状态转移方程: i天数, k交易次数, 0/1是否持有.
+ * dp[i][k][0] = max(dp[i - 1][k][0], dp[i - 1][k + 1][1] + prices[i])
+ * dp[i][k][1] = max(dp[i - 1][k + 1][0] - price[i], dp[i - 1][k][1])
+ * base case: dp[0][0] = 0 dp[0][1] = -Integer.MIN_VALUE;
+ *
+ * @since 2019-12-17 Tuesday
+ */
+public class Code_122_BestTimeBuyStock2 {
+    static int maxProfit(int[] prices) {
+        int dp_i_0 = 0, dp_i_1 = -Integer.MAX_VALUE;
+        for (int price : prices) {
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + price);
+            dp_i_1 = Math.max(dp_i_0 - price, dp_i_1);
+        }
+        return dp_i_0;
+    }
+
+    static int maxProfitV2(int[] prices) {
+        int[][] dp = new int[prices.length][prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            if (i == 0) {
+                dp[0][0] = 0;
+                dp[0][1] = -Integer.MAX_VALUE;
+            } else {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+            }
+        }
+        return dp[prices.length - 1][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 9, 3, 6, 5}));
+        System.out.println(maxProfitV2(new int[]{7, 1, 9, 3, 6, 5}));
+        System.out.println(maxProfitV2(new int[]{7, 1, 2, 3, 6, 5}));
+    }
+}
---
+++ src/leetcode/first/Code_123_BestTimeBuyStock3.java
@@ -0,0 +1,32 @@
+package leetcode.first;
+
+/**
+ * 股票买卖2, 最多交易2次,求最大利润.
+ *
+ * @since 2019-12-17 Tuesday
+ */
+public class Code_123_BestTimeBuyStock3 {
+    static int maxProfit(int[] prices) {
+        int len = prices.length;
+        int[][][] dp = new int[len][3][2];
+        dp[0][0][0] = 0;
+        dp[0][0][1] = -prices[0];
+        dp[0][1][0] = 0;
+        dp[0][1][1] = -prices[0];
+        dp[0][2][0] = 0;
+        dp[0][2][1] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            for (int j = 1; j <= 2; j++) {
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = Math.max(dp[i - 1][j - 1][0] - prices[i], dp[i - 1][j][1]);
+            }
+        }
+        return dp[len - 1][2][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4}));
+        System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(maxProfit(new int[]{7, 5, 3, 1, 0}));
+    }
+}
