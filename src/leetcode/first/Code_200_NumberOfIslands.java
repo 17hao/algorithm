916437ddde198bd83bb310eb7eabd9fce18f92f4
17hao <sqh1107@gmail.com>
2020-04-27 09:22:35 +0800

    1st: 200 && 235 2nd: 236

---
+++ src/leetcode/first/Code_200_NumberOfIslands.java
@@ -0,0 +1,41 @@
+package leetcode.first;
+
+/**
+ * 岛屿数量
+ *
+ * @since 2020-4-22 Wednesday 11:34
+ */
+public class Code_200_NumberOfIslands {
+    static int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    infect(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static void infect(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '2';
+        infect(grid, i - 1, j);
+        infect(grid, i + 1, j);
+        infect(grid, i, j - 1);
+        infect(grid, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}};
+        System.out.println(numIslands(grid));
+    }
+}
---
+++ src/leetcode/first/Code_235_LowestAncestorOfBST.java
@@ -0,0 +1,39 @@
+package leetcode.first;
+
+import tool.TreeNode;
+
+/**
+ * 二叉搜索树的最近公共节点
+ *
+ * @since 2020-4-26 19:01
+ */
+public class Code_235_LowestAncestorOfBST {
+    TreeNode res = null;
+
+    TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        helper(root, p, q);
+        return res;
+    }
+
+    private void helper(TreeNode root, TreeNode p, TreeNode q) {
+        if ((root.val - p.val) * (root.val - q.val) <= 0) {
+            res = root;
+        } else if (root.val > p.val && root.val > q.val) {
+            helper(root.left, p, q);
+        } else {
+            helper(root.right, p, q);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(20);
+        TreeNode p = new TreeNode(4);
+        TreeNode q = new TreeNode(6);
+        root.left.left = p;
+        root.left.right = q;
+        Code_235_LowestAncestorOfBST c = new Code_235_LowestAncestorOfBST();
+        System.out.println(c.lowestCommonAncestor(root, p, q));
+    }
+}
