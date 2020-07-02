0d595cbe8ad3bdab97449531cc2ed91ec824fcef
17hao <sqh1107@gmail.com>
2020-07-02 15:11:00 +0800

    2nd 200

---
+++ src/leetcode/second/Code_200_NumberOfIslands.java
@@ -0,0 +1,44 @@
+package leetcode.second;
+
+/**
+ * 岛屿数量
+ *
+ * @since 2020-7-2 Wednesday 14:52 - 15:05
+ */
+public class Code_200_NumberOfIslands {
+    static int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+        int m = grid.length, n = grid[0].length;
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    infect(grid, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    private static void infect(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1')
+            return;
+        grid[i][j] = '2';
+        infect(grid, i + 1, j);
+        infect(grid, i - 1, j);
+        infect(grid, i, j + 1);
+        infect(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        System.out.println(numIslands(grid));
+    }
+}
