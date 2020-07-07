77ed1f4fca82cbf800d861634506c011e4c2d90e
17hao <sqh1107@gmail.com>
2020-07-07 17:10:08 +0800

    update

--- leetcode-solution/149.直线上最多的点数.java
+++ leetcode-solution/149.直线上最多的点数.java
@@ -47,32 +47,25 @@
 
 // @lc code=start
 class Solution {
-    public     int maxPoints(int[][] points) {
+    public int maxPoints(int[][] points) {
         int len = points.length, max = 0;
         if (len < 3) {
             return len;
         }
         for (int i = 1; i < len; i++) {
-            if (points[i][0] != points[i - 1][0] || points[i][1] != points[i - 1][1]) {
+            if (points[i][0] != points[i - 1][0] || points[i][1] != points[i - 1][0]) {
                 break;
             }
             if (i == len - 1) {
                 return len;
             }
         }
-        Set<String> set = new HashSet<>();
         for (int i = 0; i < len; i++) {
             for (int j = i + 1; j < len; j++) {
+                int tmp = 0;
                 if (points[i][0] == points[j][0] && points[i][1] == points[j][1]) {
                     continue;
                 }
-                String key = getK(points[i][0], points[i][1], points[j][0], points[j][1]) + "@"
-                        + getB(points[i][0], points[i][1], points[j][0], points[j][1]);
-
-                if (set.contains(key)) {
-                    continue;
-                }
-                int tmp = 0;
                 for (int k = 0; k < len; k++) {
                     if (k != i && k != j) {
                         if (test(points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1])) {
@@ -80,29 +73,10 @@ class Solution {
                         }
                     }
                 }
-                max = Math.max(tmp, max);
-                set.add(key);
+                max = Math.max(max, tmp);
             }
         }
         return max + 2;
-    }
-
-    private double getK(int x1, int y1, int x2, int y2) {
-        if (x1 == x2) {
-            return Double.POSITIVE_INFINITY;
-        }
-        return (double) (y2 - y1) / (x2 - x1);
-    }
-
-    /**
-     * if (y2 == y1) { return Double.POSITIVE_INFINITY; } return (double) (x2 - x1)
-     * * (-y1) / (y2 - y1) + x1;
-     */
-    private double getB(int x1, int y1, int x2, int y2) {
-        if (x1 == x2) {
-            return Double.POSITIVE_INFINITY;
-        }
-        return y1 - (y1 - y2) / (x1 - x2) * x1;
     }
 
     private boolean test(int x1, int y1, int x2, int y2, int x, int y) {
@@ -110,4 +84,3 @@ class Solution {
     }
 }
 // @lc code=end
-
--- src/leetcode/first/Code_149_MaxPointsOnLine.java
+++ src/leetcode/first/Code_149_MaxPointsOnLine.java
@@ -49,17 +49,45 @@ class Code_149_MaxPointsOnLine {
         return max + 2;
     }
 
+    int _maxPoints(int[][] points) {
+        int len = points.length, max = 0;
+        if (len < 3) {
+            return len;
+        }
+        for (int i = 1; i < len; i++) {
+            if (points[i][0] != points[i - 1][0] || points[i][1] != points[i - 1][0]) {
+                break;
+            }
+            if (i == len - 1) {
+                return len;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                int tmp = 0;
+                if (points[i][0] == points[j][0] && points[i][1] == points[j][1]) {
+                    continue;
+                }
+                for (int k = 0; k < len; k++) {
+                    if (k != i && k != j) {
+                        if (test(points[i][0], points[i][1], points[j][0], points[j][1], points[k][0], points[k][1])) {
+                            tmp++;
+                        }
+                    }
+                }
+                max = Math.max(max, tmp);
+            }
+        }
+        return max + 2;
+    }
+
     private double getK(int x1, int y1, int x2, int y2) {
         if (x1 == x2) {
             return Double.POSITIVE_INFINITY;
         }
-        return (double) (y2 - y1) / (x2 - x1);
+        return (y2 - y1) / (x2 - x1);
     }
 
-    /**
-     * if (y2 == y1) { return Double.POSITIVE_INFINITY; } return (double) (x2 - x1)
-     * * (-y1) / (y2 - y1) + x1;
-     */
     private double getB(int x1, int y1, int x2, int y2) {
         if (x1 == x2) {
             return Double.POSITIVE_INFINITY;
@@ -73,14 +101,15 @@ class Code_149_MaxPointsOnLine {
 
     public static void main(String[] args) {
         Code_149_MaxPointsOnLine c = new Code_149_MaxPointsOnLine();
-        // System.out.println(c.maxPoints(new int[][] { { 1, 2 }, { 2, 3 } }));
-        // System.out.println(c.maxPoints(new int[][] { { 1, 1 }, { 3, 2 }, { 5, 3 }, {
-        // 4, 1 }, { 2, 3 }, { 1, 4 } }));
-        // System.out.println(
-        // c.maxPoints(new int[][] { { 1, 1 }, { 3, 2 }, { 5, 3 }, { 4, 1 }, { 2, 3 }, {
-        // 1, 4 }, { 4, 1 } }));
-        System.out.println(c.maxPoints(new int[][] { { 1, 1 }, { 1, 1 }, { 1, 1, } }));
-        System.out.println(c.maxPoints(
+
+        System.out.println(c._maxPoints(new int[][] { { 0, 0 }, { 1, 1 }, { 1, -1 } }));
+        System.out.println(c._maxPoints(new int[][] { { 1, 2 }, { 2, 3 } }));
+        System.out.println(c._maxPoints(new int[][] { { 1, 1 }, { 1, 1 }, { 2, 3 } }));
+        System.out.println(c._maxPoints(new int[][] { { 1, 1 }, { 3, 2 }, { 5, 3 }, { 4, 1 }, { 2, 3 }, { 1, 4 } }));
+        System.out.println(
+                c._maxPoints(new int[][] { { 1, 1 }, { 3, 2 }, { 5, 3 }, { 4, 1 }, { 2, 3 }, { 1, 4 }, { 4, 1 } }));
+        System.out.println(c._maxPoints(new int[][] { { 1, 1 }, { 1, 1 }, { 1, 1, } }));
+        System.out.println(c._maxPoints(
                 new int[][] { { 1, 1 }, { 1, 1 }, { 0, 0 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 7, 8 }, { 8, 9 } }));
     }
 }
