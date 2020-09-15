d7aeac730458113f036fe12e6c13bcd4a0989b98
17hao <sqh1107@gmail.com>
2020-09-15 23:53:31 +0800

    c++ 1st 236

---
+++ src/cpp-leetcode/first/Code_236_LowestAncenstorOfBT.cpp
@@ -0,0 +1,45 @@
+#include <cstdbool>
+#include <cstddef>
+
+#include "../Tree.h"
+
+class Solution {
+   private:
+    TreeNode* res;
+
+    /*
+        return true if (root == p || q) || root's left tree contains p || q
+     */
+    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) {
+            return false;
+        }
+        bool left = dfs(root->left, p, q);
+        bool right = dfs(root->right, p, q);
+        bool mid = root == p || root == q;
+        if ((mid && (left || right)) || (left && right)) {
+            res = root;
+        }
+        return mid || left || right;
+    }
+
+   public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) {
+            return NULL;
+        }
+        dfs(root, p, q);
+        return res;
+    }
+};
+
+int main(int argc, char const* argv[]) {
+    TreeNode* root = new TreeNode(1);
+    TreeNode* p = new TreeNode(2);
+    TreeNode* q = new TreeNode(3);
+    root->left = p;
+    root->right = q;
+    Solution s;
+    TreeNode* res = s.lowestCommonAncestor(root, p, q);
+    std::cout << res->val << std::endl;
+}
