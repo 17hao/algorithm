d4626d8b4d6389844f3c7e0ff27a9510af953fda
17hao <sqh1107@gmail.com>
2020-10-08 18:34:57 +0800

    c++ 1st 109

---
+++ src/cpp-leetcode/first/Code_109_ConvertSortedListToBST.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+
+#include "../List.h"
+#include "../Tree.h"
+
+/**
+ * 将有序链表转为二叉搜索树
+ *
+ * @since 2020-10-8 Thursday 17:40 - 18:34
+ */
+class Solution {
+   public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        if (head->next == nullptr) {
+            return new TreeNode(head->val);
+        }
+        ListNode* pre = new ListNode(-1);
+        pre->next = head;
+        ListNode *fast = head, *slow = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            pre = pre->next;
+        }
+        pre->next = nullptr;
+        TreeNode* root = new TreeNode(slow->val);
+        root->right = sortedListToBST(head);
+        root->left = sortedListToBST(slow->next);
+        return root;
+    }
+};
