5995f7be9fe4a8ee1d774ecd2827c282e17d47cf
17hao <sqh1107@gmail.com>
2020-10-12 14:46:51 +0800

    c++ 2nd shell sort

---
+++ src/sort/second/ShellSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/**
+ * shell sort
+ * 
+ * @since 2020-10-12 Monday 14:46
+ */
+class ShellSort {
+   public:
+    static void sort(int* arr, int count) {
+        for (int gap = count / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < count; i++) {
+                int tmp = arr[i];
+                int j = i;
+                for (; j >= gap && arr[j - gap] > arr[j]; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = tmp;
+            }
+        }
+    }
+};
+
+int main(int argc, char const* argv[]) {
+    int arr[5] = {5, 1, 4, 10, 2};
+    ShellSort::sort(arr, 5);
+    for (int i = 0; i < 5; i++) {
+        std::cout << arr[i] << " ";
+    }
+}
