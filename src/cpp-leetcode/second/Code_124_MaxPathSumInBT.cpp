5a250657e42ae41d210392055f8949b0ab9900cc
17hao <sqh1107@gmail.com>
2020-11-18 18:07:03 +0800

    c++ 2nd 124

---
+++ src/cpp-leetcode/second/Code_124_MaxPathSumInBT.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+
+#include "../Tree.h"
+
+/**
+ * max path in bianry tree
+ *
+ * @since 2020-11-18 Wednesday
+ */
+class Solution {
+    int res;
+
+    /** 返回当前根节点能为最大路径贡献的路径长度 */
+    int helper(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = std::max(helper(root->left), 0);
+        int right = std::max(helper(root->right), 0);
+        res = std::max(res, std::max(root->val, root->val + left + right));
+        return std::max(root->val, std::max(root->val + left, root->val + right));
+    }
+
+   public:
+    int maxPathSum(TreeNode* root) {
+        res = root->val;
+        helper(root);
+        return res;
+    }
+};
+
+int main(int argc, char const* argv[]) {
+    TreeNode* root = Tree::binaryTree();
+    Solution s;
+    std::cout << s.maxPathSum(root) << "\n";
+}
